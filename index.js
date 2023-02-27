cdd5994f4f11444206bf9093b55b524701a66a32
faizamsyari <89820250+faizamsyari@users.noreply.github.com>
2023-02-27 16:33:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post("/login", async(req,res)=>{
         res.send({uid:login.user.uid,data:"BERHASIL LOGIN"})
     })
     .catch(function(error){
-       res.send("ADA KESALAHAN COBA")
+       res.send("ADA KESALAHAN COBA LAGI")
        console.log(error)
     });
     // await User.firestore().collection("Data User").doc(`${data.nama}`).set(data)
